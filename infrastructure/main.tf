2db05386ce3c6cdf3e36e2d2298989a248ae4d2a
yohanse <mehabawyohanse793@gmail.com>
2025-07-03 15:24:06 +0300

    Add the triggger notification

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -198,6 +198,18 @@ resource "aws_lambda_permission" "allow-s3-to-trigger-orchestrator-lambda" {
   source_arn = aws_s3_bucket.document-input-bucket.arn
 }
 
+resource "aws_s3_bucket_notification" "orchestrator-trigger" {
+  bucket = aws_s3_bucket.document-input-bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.orchestrator-lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+    depends_on = [
+    aws_lambda_permission.allow-s3-to-trigger-orchestrator-lambda
+  ]
+}
+
 resource "aws_s3_bucket_policy" "document-input-bucket-policy" {
   bucket = aws_s3_bucket.document-input-bucket.id
 
