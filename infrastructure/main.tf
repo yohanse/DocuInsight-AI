cc249f09bb885d2df35f599faeb3055e5f5abd47
yohanse <mehabawyohanse793@gmail.com>
2025-07-03 15:15:22 +0300

    Add input and output bucket

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,3 +10,19 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "document-input-bucket" {
+  bucket        = "docuinsight-input-bucket"
+  force_destroy = true
+  tags = {
+    Name = "DocuInsight-Input-Bucket"
+  }
+}
+
+resource "aws_s3_bucket" "textract-output-bucket" {
+  bucket        = "docuinsight-textract-output-bucket"
+  force_destroy = true
+  tags = {
+    Name = "DocuInsight-Textract-Output-Bucket"
+  }
+}
