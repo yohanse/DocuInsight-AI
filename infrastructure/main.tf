6cdcc9dad061420712915eddd9b2e527faf39864
yohanse <mehabawyohanse793@gmail.com>
2025-07-03 15:23:18 +0300

    Add permission to put object to output bucket by textract

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -215,3 +215,21 @@ resource "aws_s3_bucket_policy" "document-input-bucket-policy" {
     ]
   })
 }
+
+resource "aws_s3_bucket_policy" "textract-output-bucket-policy" {
+  bucket = aws_s3_bucket.textract-output-bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "textract.amazonaws.com"
+        }
+        Action = "s3:PutObject"
+        Resource = "${aws_s3_bucket.textract-output-bucket.arn}/*"
+      }
+    ]
+  })
+}
